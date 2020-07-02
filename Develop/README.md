dec829062f0070b9be787502cf544aded44fd306
Matthew Huus <matthew.huus3@gmail.com>
2020-07-02 01:30:38 -0500

    wrote the answers to the user's input on a readme file

---
+++ Develop/README.md
@@ -0,0 +1,11 @@
+{
+	"github": "matt98h",
+	"email": "matthew.huus3@gmail.com",
+	"title": "readme generator",
+	"description": "make a readme generator",
+	"license": "MIT",
+	"installation": "npm i",
+	"test": "npm test",
+	"userInfo": "nope",
+	"contributing": "none"
+}
--- Develop/index.js
+++ Develop/index.js
@@ -1,59 +1,54 @@
-var inquirer = require("inquirer");
+var inquirer = require(`inquirer`);
+var fs = require(`fs`);
 
 // array of objects for questions for the user
 inquirer.prompt([
-{
+    {
         type: `input`,
         name: `github`,
-        message: `What is your github account?` 
-},
-{
-    type: `input`,
-    name: `email`,
-    message: `What is your email address?` 
-},
-{
-    type: `input`,
-    name: `title`,
-    message: `What is the title of your project?` 
-},
-{
-    type: `input`,
-    name: `description`,
-    message: `Write a description that describes the purpose of your project.` 
-},
-{
-    type: `list`,
-    name: `license`,
-    message: `What kind of license should your project have?`, 
-    choices: [`MIT`, `APACHE 2.0`,`GPL 3.0`,`BSD 3`,`None`]
-}, 
-{
-    type: `input`,
-    name: `installation`,
-    message: `What command should be run to install dependencies?` 
-},
-{
-    type: `input`,
-    name: `test`,
-    message: `What command hsould be run to run tests?` 
-},
-{
-    type: `input`,
-    name: `userInfo`,
-    message: `Is there anything else the user should know about using this repo?` 
-},
-{
-    type: `input`,
-    name: `contributing`,
-    message: `What does the user need to know about contributing to the repo?` 
-}
-
-
-
-
-
-// Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+        message: `What is your github account?`
+    },
+    {
+        type: `input`,
+        name: `email`,
+        message: `What is your email address?`
+    },
+    {
+        type: `input`,
+        name: `title`,
+        message: `What is the title of your project?`
+    },
+    {
+        type: `input`,
+        name: `description`,
+        message: `Write a description that describes the purpose of your project.`
+    },
+    {
+        type: `list`,
+        name: `license`,
+        message: `What kind of license should your project have?`,
+        choices: [`MIT`, `APACHE 2.0`, `GPL 3.0`, `BSD 3`, `None`]
+    },
+    {
+        type: `input`,
+        name: `installation`,
+        message: `What command should be run to install dependencies?`
+    },
+    {
+        type: `input`,
+        name: `test`,
+        message: `What command hsould be run to run tests?`
+    },
+    {
+        type: `input`,
+        name: `userInfo`,
+        message: `Is there anything else the user should know about using this repo?`
+    },
+    {
+        type: `input`,
+        name: `contributing`,
+        message: `What does the user need to know about contributing to the repo?`
+    }
 
 
 
@@ -63,12 +58,14 @@ inquirer.prompt([
 
 
 
-]).then(function(data, err)  {
-if(err){
-    return console.log (err);
-}
-console.log(`Success!: `, data);
 
+]).then(function (data) {
+    fs.writeFile("README.md", JSON.stringify(data, null, '\t'),function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(`Success!: `, data);
+    })
 
 
 })
